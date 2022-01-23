3d20ac5b46b9ba25ddca768d50a98e9f57679c1f
Pei Qi Tea <moneysdew@gmail.com>
2022-01-24 06:48:33 +0900

    attempt: count vowels

---
+++ strings/vowels/index.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+function vowels(str) {
+    // Method 1: Use Regex
+    const vowelsRegex = /[aeiou]/gi
+    const matches = str.match(vowelsRegex);
+    return matches ? matches.length : 0;
+}
+
+module.exports = vowels;
